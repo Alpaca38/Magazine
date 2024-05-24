d8536c46ffbeacf5ea1f3a3c61c34ec193e4a812
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-24 17:15:32 +0900

    Initial Commit

---
+++ Magazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magazine
+//
+//  Created by 조규연 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
