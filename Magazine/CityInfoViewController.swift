99acc9c0b42ff223dc99c01dfdae32a87f0c4487
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-29 17:22:12 +0900

    segment 선택에 따라 다른 도시 보여주는 기능 추가

--- Magazine/CityInfoViewController.swift
+++ Magazine/CityInfoViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class CityInfoViewController: UIViewController {
     
-    let list = CityInfo.city
+    var list = CityInfo.city
+    var filteredList: [City] = []
 
     @IBOutlet var searchCityBar: UISearchBar!
     @IBOutlet var categorySegControl: UISegmentedControl!
@@ -18,9 +19,40 @@ class CityInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        filteredList = list
+        
         configureNavi(title: "인기 도시")
 
         configureTableView()
+        
+        configureSegment()
+    }
+    
+    func configureSegment() {
+        let categorys = ["모두","국내","해외"]
+        
+        for (index, category) in categorys.enumerated() {
+            categorySegControl.setTitle(category, forSegmentAt: index)
+        }
+        
+        categorySegControl.addTarget(self, action: #selector(segmentValueChanged), for: .valueChanged)
+        
+    }
+    
+    @objc func segmentValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            filteredList = list
+            tableView.reloadData()
+        case 1:
+            filteredList = list.filter({ $0.domestic_travel == true })
+            tableView.reloadData()
+        case 2:
+            filteredList = list.filter({ $0.domestic_travel == false })
+            tableView.reloadData()
+        default:
+            return
+        }
     }
     
     func configureTableView() {
@@ -38,12 +70,12 @@ class CityInfoViewController: UIViewController {
 
 extension CityInfoViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        list.count
+        filteredList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CityInfoTableViewCell", for: indexPath) as! CityInfoTableViewCell
-        cell.configure(data: list[indexPath.row])
+        cell.configure(data: filteredList[indexPath.row])
         
         return cell
     }
